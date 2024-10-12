d45bd3fe9416e7bdd9d84cc05506d74e8b28f44e
Katsiaryna Kulikovich <1902katerina2011@gmail.com>
2024-10-12 20:19:12 +0200

    added tags

--- file1.tf
+++ file1.tf
@@ -7,4 +7,10 @@ provider "aws" {
 resource "aws_instance" "terraform_lesson" {
   ami           = "ami-097c5c21a18dc59ea"
   instance_type = "t3.micro"
+
+  tags = {
+    Name    = "1st_instance"
+    Owner   = "kulerock"
+    Project = "Terraform Lessons"
+  }
 }
