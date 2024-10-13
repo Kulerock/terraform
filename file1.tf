9f50c945aeb1e21262a407216023621a47e46c0a
Katsiaryna Kulikovich <1902katerina2011@gmail.com>
2024-10-13 13:51:10 +0200

    corrected syntax

--- file1.tf
+++ file1.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "terraform_lesson" {
 #!/bin/bash
 yum -y update
 yum -y install httpd
-myip=`curl http://192.168.56.104/latest/meta-data/local-ipv4`
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
