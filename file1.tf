73f43fc5cfa1c16cffe644fe593d3081711cc765
Katsiaryna Kulikovich <1902katerina2011@gmail.com>
2024-10-16 17:04:38 +0200

    Added ASG, ELB

--- file1.tf
+++ file1.tf
@@ -2,32 +2,14 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "terraform_lesson" {
-  ami                    = "ami-097c5c21a18dc59ea"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.first_group.id]
-  user_data              = templatefile("user_data.sh.tpl",{
-    f_name               = "Kate",
-    l_name               = "Kulikovich",
-    names                = ["Vasya","Kolya","Petya","Masha"]
-  })
-  metadata_options {
-    http_tokens = "optional"
-  }
-
-  tags = {
-    Name    = "first_instance"
-    Owner   = "kulerock"
-    Project = "Terraform Lessons"
-  }
-}
+data "aws_availability_zones" "available" {}
 
 resource "aws_security_group" "first_group" {
   name        = "Dynamic Security Group"
   description = "Allow SSH inbound traffic"
   
   dynamic "ingress" {
-    for_each = ["80", "443", "8080", "1541", "9092"]
+    for_each = ["80", "443"]
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -46,3 +28,106 @@ resource "aws_security_group" "first_group" {
     Name    = "Dynamic Security Group"
   }
 }
+
+resource "aws_launch_configuration" "web" {
+  name            = "WebServer-Highly-Available"
+  image_id        = "ami-097c5c21a18dc59ea"
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.first_group.id]
+  user_data       = file("user_data.sh.tlp",{
+     f_name               = "Kate",
+     l_name               = "Kulikovich",
+     names                = ["Vasya","Kolya","Petya","Masha"]
+   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                      = "WebServer-Highly-Available-ASG"
+  launch_configuration      = aws_launch_configuration.web.name
+  max_size                  = 1
+  min_size                  = 1
+  min_elb_capacity          = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+
+  vpc_zone_identifier      = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  load_balancers           = [aws_elb.web.name]
+
+  dynamic "tag" {
+    for_each = {
+        Name   = "WebServer-in-ASG"
+        Owner  = "Kate"
+        TAGKEY = "TAGVALUE"
+    }
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true  
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_elb" "web" {
+  name               = "WebServer-HA-ELB"
+  availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  security_groups    = [aws_security_group.first_group.id]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = 80
+    instance_protocol = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 10
+  } 
+    tags = {
+      Name = "WebServer-Highly-Available-ELB"
+  }
+}
+
+  resource "aws_default_subnet" "default_az1" {
+    availability_zone = data.aws_availability_zones.available.names[0]
+  }
+
+  resource "aws_default_subnet" "default_az2" {
+    availability_zone = data.aws_availability_zones.available.names[1]
+  }
+
+output "web_loadbalancer_url" {
+  value = aws_elb.web.dns_name
+}
+
+# resource "aws_instance" "terraform_lesson" {
+#   ami                    = "ami-097c5c21a18dc59ea"
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [aws_security_group.first_group.id]
+#   user_data              = templatefile("user_data.sh.tpl",{
+#     f_name               = "Kate",
+#     l_name               = "Kulikovich",
+#     names                = ["Vasya","Kolya","Petya","Masha"]
+#   })
+#   metadata_options {
+#     http_tokens = "optional"
+#   }
+
+#   tags = {
+#     Name    = "first_instance"
+#     Owner   = "kulerock"
+#     Project = "Terraform Lessons"
+#   }
+
+# }
+
